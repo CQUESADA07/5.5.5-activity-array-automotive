5c07af9ce2ea812e3a3107923f0db04dd3a28632
Carlo <carloquesada68@yahoo.com>
2023-10-24 13:04:44 -0700

    finished

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,35 +1,60 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle").Vehicle
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-
-//After you write the derived Car class, you should test it out.
-
-//Note: You can code your derived Car class here or make a file named index.js and do it there.
-
-
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
-
-
-
-
-
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
 
 
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " all seats are taken.");
+    
+            }
+        } else {
+            console.log(this.model + " " + this.make + " the car is full");
+        }
+    }
 
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("fuel empty");
+            return this.started = false;
+        }
+    }
 
+    scheduleService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
 
+}
 
+let newCar = new newCar('mercury', 'sedan', '2023', 'black', 45000)
 
-//TO DO: Creating Instances and Testing Them
+newCar.start()
+newCar.loadPassenger(5)
+newCar.stop()
+newCar.checkService()
 
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
+console.log(newCar)
